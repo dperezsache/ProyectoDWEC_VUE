cbdda4325817ebab7a48cfa761e2fc1bfee39041
David Pérez <dperezsache.guadalupe@alumnado.fundacionloyola.net>
2023-01-10 15:48:39 +0100

    Guía de estilo y logos

---
+++ src/www/js/modelos/modelo.js
@@ -0,0 +1,15 @@
+/**
+	@file Contiene el modelo de la aplicación.
+	@author David Pérez Saché <dperezsache.guadalupe@alumnado.fundacionloyola.net>
+	@license GPL-3.0-or-later
+**/
+
+/**
+	Clase Modelo.
+	Gestiona los datos de la aplicación.
+**/
+export class modelo {
+    constructor() {
+
+    }
+}
